adcb08525e495ba69b4714d6391ce64d96fb7261
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-01-31 17:50:05 +0100

    inverto la condizione fizzbuzz affinche abbia la priorita,  non viene visualizzato perche viene sovrascritto

--- script.js
+++ script.js
@@ -2,13 +2,16 @@
 // for (let i = 0; i < 10; i++)
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
-        console.log('FIZZ');
+    if (i % 15 === 0) {
+        console.log('FIZZBUZZ');
     } else if (i % 5 === 0) {
         console.log('BUZZ')
-    } else if (i % 15 === 0) {
-        console.log('FIZZBUZZ')
+    } else if (i % 3 === 0) {
+        console.log('FIZZ')
     } else {
         console.log(i)
     }
 }
+
+
+
