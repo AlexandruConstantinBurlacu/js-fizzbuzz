17b1e70813c7b6cfc9d1d07315b57b0d7d25d4d5
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-01-31 17:44:56 +0100

    inserimento regola fizzbuzz

--- script.js
+++ script.js
@@ -6,5 +6,7 @@ for (let i = 1; i <= 100; i++) {
         console.log('FIZZ');
     } else if (i % 5 === 0) {
         console.log('BUZZ')
-    }
+    } else if (i % 15 === 0) {
+        console.log('FIZZBUZZ')
+    } 
 }
